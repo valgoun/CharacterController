e944c301626f32c9a0487707d70e79c604dfccd0
valgoun <valgoun@gmail.com>
2017-06-26 17:07:14 +0200

    Unity Init

---
+++ Assets/Scritps/Character.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float Speed = 5f;
+    public float JumpHeight = 2f;
+    public float Gravity = -9.81f;
+    public float GroundDistance = 0.2f;
+    public float DashDistance = 5f;
+    public LayerMask Ground;
+    public Vector3 Drag;
+
+    private CharacterController _controller;
+    private Vector3 _velocity;
+    private bool _isGrounded = true;
+    private Transform _groundChecker;
+
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _groundChecker = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+        if (_isGrounded && _velocity.y < 0)
+            _velocity.y = 0f;
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * Speed);
+        if (move != Vector3.zero)
+            transform.forward = move;
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+            _velocity.y += Mathf.Sqrt(JumpHeight * -2f * Gravity);
+        if (Input.GetButtonDown("Dash"))
+        {
+            Debug.Log("Dash");
+            _velocity += Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * Drag.x + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * Drag.z + 1)) / -Time.deltaTime)));
+        }
+
+
+        _velocity.y += Gravity * Time.deltaTime;
+
+        _velocity.x /= 1 + Drag.x * Time.deltaTime;
+        _velocity.y /= 1 + Drag.y * Time.deltaTime;
+        _velocity.z /= 1 + Drag.z * Time.deltaTime;
+
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scritps/RigidbodyCharacter.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyCharacter : MonoBehaviour
+{
+
+    public float Speed = 5f;
+    public float JumpHeight = 2f;
+    public float GroundDistance = 0.2f;
+    public float DashDistance = 5f;
+    public LayerMask Ground;
+
+    private Rigidbody _body;
+    private Vector3 _inputs = Vector3.zero;
+    private bool _isGrounded = true;
+    private Transform _groundChecker;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        _groundChecker = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, Ground, QueryTriggerInteraction.Ignore);
+
+
+        _inputs = Vector3.zero;
+        _inputs.x = Input.GetAxis("Horizontal");
+        _inputs.z = Input.GetAxis("Vertical");
+        if (_inputs != Vector3.zero)
+            transform.forward = _inputs;
+
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        if (Input.GetButtonDown("Dash"))
+        {
+            Vector3 dashVelocity = Vector3.Scale(transform.forward, DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime), 0, (Mathf.Log(1f / (Time.deltaTime * _body.drag + 1)) / -Time.deltaTime)));
+            _body.AddForce(dashVelocity, ForceMode.VelocityChange);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        _body.MovePosition(_body.position + _inputs * Speed * Time.fixedDeltaTime);
+    }
+}
